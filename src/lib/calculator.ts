d9ac110bd7bb8f1484f71a34f810594bc60048d5
Avram Walden <aviemet@gmail.com>
2021-01-31 14:26:50 -0800

    Adds method for handling input of a value key

--- src/lib/calculator.ts
+++ src/lib/calculator.ts
@@ -1,5 +1,19 @@
-class Calculator {
+const testNumbers = /[0-9.]/g
 
+const Calculator = {
+	inputNumber: (display: string, value: string) => {
+		if(!value.match(testNumbers)) return display
+
+		if((value === '0' && display === '0') || value === '.' && display.includes('.')) {
+			return display
+		}
+
+		if(display === '0') {
+			return value
+		}
+
+		return `${display}${value}`
+	}
 }
 
 export default Calculator
